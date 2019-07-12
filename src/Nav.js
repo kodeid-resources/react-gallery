c456ccf710b764bf0d7a65758748d6f81cd277be
Riza Fahmi <rizafahmi@gmail.com>
2019-07-12 13:33:45 +0700

    Finish Nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import styles from './nav-styles.js';
+
 function Nav(props) {
   return (
-    <div>
-      <button>&#10094;</button>
-      <button>&#10095;</button>
+    <div style={styles.root}>
+      <button style={styles.prev} onClick={props.onPrevious}>
+        &#10094;
+      </button>
+      <button style={styles.next} onClick={props.onNext}>
+        &#10095;
+      </button>
     </div>
   );
 }
